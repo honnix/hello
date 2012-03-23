e1700584507a7205e5ffe963114ab82adff4c424
ehonlia <hongxin.liang@ericsson.com>
2012-03-23 09:16:35 +0800

    typed actor demo

--- src/main/scala/TypedActorDemo.scala
+++ src/main/scala/TypedActorDemo.scala
@@ -1,5 +1,7 @@
 import akka.actor._
 import akka.actor.{TypedActor, TypedProps}
+import akka.dispatch.Await
+import akka.util.duration._
 
 object TypedActorDemo extends App {
   val system = ActorSystem("TypedActorSystem")
@@ -8,6 +10,9 @@ object TypedActorDemo extends App {
 
   mySquarer.squareDontCare(10)
   println(mySquarer.squareNowPlease(10))
+  println(mySquarer.squareNow(10))
 
+  val f = mySquarer.square(10)
+  println(Await.result(f, 1 second))
   system.shutdown
 }
